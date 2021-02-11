5a7389b97fbb01a7b37ec8ed204f68a55a4e87c7
wes-public-apps <murraywj@yahoo.com>
2021-02-11 07:29:10 -0800

    fixed issues with simple insert in order and implemented binary search insertion

--- agorithms/inserting.py
+++ agorithms/inserting.py
@@ -5,14 +5,47 @@
 #simple insert
 #O(n)
 def insertInOrder(arr,val):
+    #handle edge cases
+    if(arr==None): return [val]
+
+    #handle normal cases
     ind=0
     arr.append(val)
     while(arr[ind]<val): ind+=1
-
     if(ind<len(arr)-1): 
         arr.insert(ind,val)
         del arr[len(arr)-1]
+    
+    return arr
 
-
+#insert using binary search logic
+#O(log2n)
 def insertInOrderBinary(arr,val):
-    return None
+    #handle edge cases
+    if(arr==None): return [val]
+    if(arr==[]): return [val]
+
+    #handle normal cases
+    arrLen=len(arr)
+    start=0
+    end=arrLen-1
+    midPoint=0
+    while end!=start:
+        midPoint=(end+start)//2
+
+        #exit if val is equal to array value in question
+        if(arr[midPoint]==val):
+            break
+        
+        #determine array section to check next
+        if(arr[midPoint]<val):
+            start=midPoint+1
+        else:
+            end=midPoint-1
+
+    if arr[start]>val:
+        arr.insert(start,val)
+    else:
+        arr.insert(start+1,val)
+
+    return arr
