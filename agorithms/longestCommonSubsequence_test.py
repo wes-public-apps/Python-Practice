265caf10219c9612e18db9f698fd60632952f917
wes-public-apps <murraywj@yahoo.com>
2021-02-11 09:26:56 -0800

    initialize test file for least common sequence

---
+++ agorithms/longestCommonSubsequence_test.py
@@ -0,0 +1,15 @@
+# Wesley Murray
+# 2/11/2021
+# For testing the longest common subsequence
+
+import unittest
+import longestCommonSubsequence as lcs
+
+class TestLongestCommonSubsequence(unittest.TestCase):
+
+    #This method is for testing longest common subsequence algorithm
+    def test_LongestCommonSequence(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
