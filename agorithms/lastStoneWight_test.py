4aa0aad51d4a00ae7cda18bd0e1b2161465b2726
wes-public-apps <murraywj@yahoo.com>
2021-02-11 07:33:40 -0800

    initialize unit test for last stone weight problem

--- agorithms/lastStoneWight_test.py
+++ agorithms/lastStoneWight_test.py
@@ -1,2 +1,16 @@
+# Wesley Murray
+# 2/11/2021
+# This script is for testing my code for the last rock size problem.
 
+import unittest
+import lastStoneWeight as lsw
+
+class LastRockSizeTest(unittest.TestCase):
+
+    #test program that determines final rock size
+    def test_rockSize(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
 
