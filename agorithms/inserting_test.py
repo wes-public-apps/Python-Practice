03fe26a32b7208c5827fb312cfe705712f6676ba
wes-public-apps <murraywj@yahoo.com>
2021-02-11 07:17:11 -0800

    add test cases for binary search

--- agorithms/inserting_test.py
+++ agorithms/inserting_test.py
@@ -10,12 +10,11 @@ class TestInserting(unittest.TestCase):
     #test the method that inserts a value into a sorted list 
     #using binary search
     def test_insertInOrderBinary(self):
-        self.assertTrue(True)
-        # self.assertEqual(Insertion.insertInOrderBinary(None,3),[3])
-        # self.assertEqual(Insertion.insertInOrderBinary([],3),[3])
-        # self.assertEqual(Insertion.insertInOrderBinary([1,2,4,5,7],3),[1,2,3,4,5,7])
-        # self.assertEqual(Insertion.insertInOrderBinary([8,10,13],3),[3,8,10,13])
-        # self.assertEqual(Insertion.insertInOrderBinary([8,10,13],14),[8,10,13,14])
+        self.assertEqual(Insertion.insertInOrderBinary(None,3),[3])
+        self.assertEqual(Insertion.insertInOrderBinary([],3),[3])
+        self.assertEqual(Insertion.insertInOrderBinary([1,2,4,5,7],3),[1,2,3,4,5,7])
+        self.assertEqual(Insertion.insertInOrderBinary([8,10,13],3),[3,8,10,13])
+        self.assertEqual(Insertion.insertInOrderBinary([8,10,13],14),[8,10,13,14])
 
     #test the method that inserts a value into a sorted list 
     def test_insertInOrder(self):
