b4970a26ae89281f1fe46916ceb453d73b7fe330
wes-public-apps <murraywj@yahoo.com>
2021-02-11 07:38:56 -0800

    add test cases for last stone weight problem

--- agorithms/lastStoneWight_test.py
+++ agorithms/lastStoneWight_test.py
@@ -9,7 +9,13 @@ class LastRockSizeTest(unittest.TestCase):
 
     #test program that determines final rock size
     def test_rockSize(self):
-        self.assertTrue(True)
+        self.assertEqual(lsw.determineSizeOfLastRock(None),0)
+        self.assertEqual(lsw.determineSizeOfLastRock([]),0)
+        self.assertEqual(lsw.determineSizeOfLastRock([7]),7)
+        self.assertEqual(lsw.determineSizeOfLastRock([7,7]),0)
+        self.assertEqual(lsw.determineSizeOfLastRock([7,3,7]),3)
+        self.assertEqual(lsw.determineSizeOfLastRock([7,3,7,1,1]),1)
+        self.assertEqual(lsw.determineSizeOfLastRock([2,7,4,1,8,1]),1)
 
 if __name__ == "__main__":
     unittest.main()
