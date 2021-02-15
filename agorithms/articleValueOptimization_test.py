868de401979549c905ed1a37482fe1f0634cbc0f
wes-public-apps <murraywj@yahoo.com>
2021-02-15 06:57:39 -0800

    initialize test file for article value optimization problem

---
+++ agorithms/articleValueOptimization_test.py
@@ -0,0 +1,14 @@
+# Wesley Murray
+# 2/15/2020
+# This file is for testing the program that optimizes article value
+
+import unittest
+
+class TestArticleValueOptimization(unittest.TestCase):
+
+    #test brute force solution
+    def test_bruteforce(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
